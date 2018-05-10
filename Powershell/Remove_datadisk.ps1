0759947880f4f62b255b3aa66798498871a3711f
Ben Hitchcock <33057042+bender9000@users.noreply.github.com>
2018-05-10 11:11:12 +0100

    all gone

--- Powershell/Remove_datadisk.ps1
+++
@@ -1,7 +0,0 @@
-﻿
-
-$VirtualMachine = Get-AzureRmVM -ResourceGroupName "UK-UKS-PROD-INF-001-RG" -Name "UK-SUH-DC001"
-Remove-AzureRmVMDataDisk -VM $VirtualMachine -Name "UK-SUH-DC011-DataDisk"
-Update-AzureRmVM -ResourceGroupName "UK-UKS-PROD-INF-001-RG" -VM $VirtualMachine
-
-
